fb0f4d3de05f2a4fe1b4dc1d2562b8f49ff30fcb
jot-s-bindra <jotsbindra03@gmail.com>
2023-12-09 18:51:56 +0530

    Initial commit

---
+++ CP/1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t,n; 
+	cin>>t;
+	while(t--){
+		cin>>n;
+		vector<int> v(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>v[i];
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<v[i]<<" ";
+		}
+		cout<<endl;
+	}
+}
---
+++ CP/1.py
@@ -0,0 +1,9 @@
+lst = []
+
+n = int(input ("shukla bsdk"))
+
+for i in range(0,n):
+    ele = int(input())
+    lst.append(ele)
+
+print(lst)
